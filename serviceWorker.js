fe7de3e6ef33fe850681cebf8cf15f6068a42ec6
P R Robinson <p.r.robinson@outlook.com>
2022-02-14 22:25:53 +0000

    PWA Manifest & support files (custom icon needed)

---
+++ serviceWorker.js
@@ -0,0 +1,24 @@
+const staticPrayerBookOnline = "dev-pbo-site-v1"
+const assets = [
+    "/",
+    "/index.html",
+    "/css/style.css",
+    "js/app.js",
+    "services/The-Lords-Supper-or-Holy-Communion.html",
+]
+
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+        caches.open(staticPrayerBookOnline).then(cache => {
+            cache.addAll(assets)
+        })
+    )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request)
+        })
+    )
+})
