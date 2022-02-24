1ec99bbb31f322890b549f37c0e5db5260f9dd66
P R Robinson <p.r.robinson@outlook.com>
2022-02-24 18:27:45 +0000

    Correct path when caching files

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,13 +1,13 @@
 const staticPrayerBookOnline = "dev-pbo-site-v1"
 const assets = [
-    "./prayerbook-online/",
-    "./prayerbook-online/favicon.ico",
-    "./prayerbook-online/favicon.svg",
-    "./prayerbook-online/index.html",
-    "./prayerbook-online/css/style.css",
-    "./prayerbook-online/js/app.js",
-    "./prayerbook-online/images/EucharistElementsHeader-noFont.svg",
-    "./prayerbook-online/services/The-Lords-Supper-or-Holy-Communion.html",
+    "./",
+    "./favicon.ico",
+    "./favicon.svg",
+    "./index.html",
+    "./css/style.css",
+    "./js/app.js",
+    "./images/EucharistElementsHeader-noFont.svg",
+    "./services/The-Lords-Supper-or-Holy-Communion.html",
 ]
 
 self.oninstall = function(event) {
