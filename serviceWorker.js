1f81a9d6510937fe3fbd000091837c9cd09c9a49
P R Robinson <p.r.robinson@outlook.com>
2022-02-28 21:56:41 +0000

    ServiceWorker to network first

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,11 +1,14 @@
 const staticPrayerBookOnline = "dev-pbo-site-v1"
 const assets = [
     "./",
+    "./index.html",
+    "./calendar.html",
     "./favicon.ico",
     "./favicon.svg",
-    "./index.html",
     "./css/style.css",
     "./js/app.js",
+    "./js/computus.js",
+    "./js/dates.json",
     "./images/EucharistElementsHeader-noFont.svg",
     "./services/The-Lords-Supper-or-Holy-Communion.html",
 ]
@@ -32,7 +35,7 @@ self.onactivate = function(event) {
     );
 }
 
-self.onfetch = function(event) {
+/* self.onfetch = function(event) {
     event.respondWith(
         caches.match(event.request).then(function(response) {
             console.log(event.request.url);
@@ -44,6 +47,18 @@ self.onfetch = function(event) {
                     return caches.match('./fallback.html');
                 })
             });
+        })
+    ); */
+
+self.onfetch = function(event) {
+    event.respondWith(
+        fetch(event.request).then(function(response) {
+            return caches.open(staticPrayerBookOnline).then(function(cache) {
+                cache.put(event.request, response.clone());
+                return response;
+            }).catch(() => {
+                    return caches.match(event.request);
+            })
         })
     );
 }
