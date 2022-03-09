3b2a6380a290ee3941ef6db5aa3ad17c092da048
P R Robinson <p.r.robinson@outlook.com>
2022-03-09 22:13:22 +0000

    Lookup names of books & convert '1' into '1st'...

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+var bookLookup = { 
+    "1 Cor": "1<sup>st</sup> Corinthians",
+    "2 Cor": "2<sup>nd</sup> Corinthians",
+    "Matth": "St Matthew"
+};
+
 function toggleHamburger() {
     var x = document.getElementById("navbar");
     if(x.className === "topnav") {
@@ -5,4 +11,22 @@ function toggleHamburger() {
     } else {
         x.className = "topnav";
     }
+}
+
+function numberToReference(number) {
+    switch(number.substr(number.length - 1)) {
+        case "1":
+            number += "<sup>st</sup>";
+          break;
+        case "2":
+            number += "<sup>nd</sup>";
+          break;
+        case "3":
+            number += "<sup>rd</sup";
+          break;
+        default:
+            number += "<sup>th</sup>";
+      }
+
+      return number;
 }
