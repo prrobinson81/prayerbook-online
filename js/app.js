78ed3edd6f1e167e4703e9109c90215be5265b6c
P R Robinson <p.r.robinson@outlook.com>
2022-03-10 22:12:07 +0000

    Fixed numbering and added books

--- js/app.js
+++ js/app.js
@@ -1,7 +1,33 @@
 var bookLookup = { 
+    "Isaiah": "Isaiah",
+    "Joel": "Joel",
+    "S.Matth": "St Matthew",
+    "S.Mark": "St Mark",
+    "S.Luke": "St Luke",
+    "S.John": "St John",
+    "Acts": "Acts",
+    "Rom": "Romans",
     "1 Cor": "1<sup>st</sup> Corinthians",
     "2 Cor": "2<sup>nd</sup> Corinthians",
-    "Matth": "St Matthew"
+    "Gal": "Galations",
+    "Ephes": "Ephesians",    
+    "Philip": "Philippians",
+    "Coloss": "Colossians",
+    "1 Thess": "1<sup>st</sup> Thessalonians",
+    "2 Thess": "2<sup>nd</sup> Thessalonians",
+    "1 Tim": "1<sup>st</sup> Timothy",
+    "2 Tim": "2<sup>nd</sup> Timothy",
+    "Tit": "Titus",
+    "Phil": "Philemon",
+    "Hebr": "Hebrews",
+    "S.James": "St James",
+    "1 S.Peter": "1<sup>st</sup> St Peter",
+    "2 S.Peter": "2<sup>nd</sup> St Peter",
+    "1 S.John": "1<sup>st</sup> St John",
+    "2 S.John": "2<sup>nd</sup> St John",
+    "3 S.John": "3<sup>rd</sup> St John",
+    "Jud": "Jude",
+    "Rev": "Revelation"
 };
 
 function toggleHamburger() {
@@ -14,6 +40,10 @@ function toggleHamburger() {
 }
 
 function numberToReference(number) {
+    if(number > 10 && number < 20) {
+        return number += "<sup>th</sup>";
+    }
+
     switch(number.substr(number.length - 1)) {
         case "1":
             number += "<sup>st</sup>";
@@ -22,11 +52,11 @@ function numberToReference(number) {
             number += "<sup>nd</sup>";
           break;
         case "3":
-            number += "<sup>rd</sup";
+            number += "<sup>rd</sup>";
           break;
         default:
             number += "<sup>th</sup>";
-      }
+    }
 
-      return number;
+    return number;
 }
